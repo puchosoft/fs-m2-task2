974f8f3222834aa364cc31b840a5a7986e918d6f
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-04-29 10:36:55 -0300

    Finalizada la Tarea 2

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,15 +1,14 @@
 // **************************************************************
 // Agrega una cabecera a un contenedor <header> con id="cabecera"
 // **************************************************************
-
 var cabecera = document.getElementById("cabecera");
 
 cabecera.innerHTML = '\
 <div class="row align-items-center">\
-  <div class="col-md-1">\
-    <img src="img/tgif-logo.jpg" class="img-fluid rounded-circle" alt="tgif-logo">\
+  <div class="col-md-2">\
+    <a href="home.html"><img src="img/tgif-logo.png" class="img-fluid rounded" alt="tgif-logo"></a>\
   </div>\
-  <div class="col-md-9 text-center">\
+  <div class="col-md-8 text-center">\
     <h2>Transparent Government in Fact</h2>\
   </div>\
   <div class="col-md-2 text-right">\
@@ -18,6 +17,34 @@ cabecera.innerHTML = '\
   </div>\
 </div>\
 ';
+
+// ***************************************************************************
+// Agrega un contenedor con checkboxs y dropdown para filtrar con id="filtros"
+// ***************************************************************************
+var filtros = document.getElementById("filtros");
+if(filtros != null){
+  filtros.innerHTML = '\
+  <div class="row align-items-center">\
+    <div class="col-md-9">\
+      Filter by Party:\
+      <label class="mx-2" for="republican">\
+        <input checked type="checkbox" id="republican" name="party" value="R" /> Republican</label>\
+      <label class="mx-2" for="democrat">\
+        <input checked type="checkbox" id="democrat" name="party" value="D" /> Democrat</label>\
+      <label class="mx-2" for="independent">\
+        <input checked type="checkbox" id="independent" name="party" value="I" />\ Independent</label>\
+    </div>\
+    <div class="col-md-2 text-right">\
+      <span>Filter by State</span>\
+    </div>\
+    <div class="col-md-1">\
+      <select class="form-control px-0" id="dropStates">\
+        <option value="">All</option>\
+      </select>\
+    </div>\
+  </div>\
+  ';
+}
 
 // ***************************************************
 // Agrega un pie a un contenedor <footer> con id="pie"
