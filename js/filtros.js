abfa562dd00f739a7ae1818e70bc2493d4456436
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-04-29 08:20:17 -0300

    Finalizado el punto 11

--- js/filtros.js
+++ js/filtros.js
@@ -1,6 +1,21 @@
+// Crea un array con los miembros de la camara
+var miembros = data.results[0].members;
+
 // Agrega listeners que atiendan los cambios de los checkboxes
 var query = document.querySelectorAll('input[name=party]');
 query.forEach(input => input.onchange = filtraYMuestraTabla);
+
+// Crea un array con todos los estados existentes en la base
+var states = miembros.map(miembro => miembro.state);
+
+// Reduce el array a los estados unicos y los ordena
+states = states.filter((state, i, array) => array.indexOf(state) === i);
+states.sort();
+
+// Genera las opciones del dropdown "Filter by State"
+setDropStates(states);
+
+filtraYMuestraTabla();
 
 function filtraYMuestraTabla(){
   // Lee los checkboxes seleccionados del "Filter by Party"
@@ -9,9 +24,6 @@ function filtraYMuestraTabla(){
   // Crea un array con los valores de los checkboxes seleccionados
   var partidoSeleccionado = Array.from(query).map(element => element.value)
 
-  // Crea un array con los miembros de la camara
-  var miembros = data.results[0].members;
-
   // Funcion de filtrado de miembros segun partidos seleccionados
   var partyFilter = miembro => {
     if(partidoSeleccionado.indexOf(miembro.party) > -1){
@@ -19,11 +31,32 @@ function filtraYMuestraTabla(){
     }
   }
 
-  // Reduce el array de miembros dejando los seleccionados por el filtro
-  miembros = miembros.filter(partyFilter);
+  // Reduce el array de miembros dejando los seleccionados por el partyFilter
+  mFiltrados = miembros.filter(partyFilter);
+
+  // Si no esta seleccionado "All", filtra por estado
+  var state = document.getElementById("dropStates").value;
+  if (state != ""){
+    mFiltrados = mFiltrados.filter(m => m.state == state);
+  }
 
   // Muestra la tabla
-  setTable(miembros);
+  setTable(mFiltrados);
 }
 
-filtraYMuestraTabla();
+function setDropStates(states){
+  var dropStates = document.getElementById("dropStates");
+
+  dropStates.onchange = filtraYMuestraTabla;
+
+  states.forEach(state => {
+    var option = document.createElement("option");
+    option.value = state;
+    option.innerText = state;
+    dropStates.appendChild(option);
+  })
+}
+
+function nosequehacemos(){
+  console.log("cambio de state");
+}
--- js/header-footer.js
+++ js/header-footer.js
@@ -6,17 +6,17 @@ var cabecera = document.getElementById("cabecera");
 
 cabecera.innerHTML = '\
 <div class="row align-items-center">\
-    <div class="col-md-1">\
-        <img src="img/tgif-logo.jpg" class="img-fluid rounded-circle" alt="tgif-logo">\
-    </div>\
-    <div class="col-md-9 text-center">\
-        <h2>Transparent Government in Fact</h2>\
-    </div>\
-    <div class="col-md-2 text-right">\
-        <span class="fa fa-envelope"></span>\
-        <a href="mailto:info@tgif.net">info@tgif.net</a>\
-    </div>\
+  <div class="col-md-1">\
+    <img src="img/tgif-logo.jpg" class="img-fluid rounded-circle" alt="tgif-logo">\
   </div>\
+  <div class="col-md-9 text-center">\
+    <h2>Transparent Government in Fact</h2>\
+  </div>\
+  <div class="col-md-2 text-right">\
+    <span class="fa fa-envelope"></span>\
+    <a href="mailto:info@tgif.net">info@tgif.net</a>\
+  </div>\
+</div>\
 ';
 
 // ***************************************************
